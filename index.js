ad1af9028454f18b666442972cb2fa2d8b837465
justinhelmer <jhelmer25@gmail.com>
2016-02-12 17:00:19 -0800

    version bump

--- index.js
+++ index.js
@@ -7,18 +7,16 @@
   'use strict';
 
   var map = require('map-stream');
-  var through2 = require('through2');
 
   /**
    * Through stream with a simple passthrough after a specified duration.
    *
    * @name wait
    * @param {number} timeout - The duration to wait, in milliseconds.
-   * @param {object} [options] - The options used to modify the behavior of the function.
    * @param {boolean} [options.through2] - If set to `true`, will use through2 streams instead of through streams.
    * @return {stream} - The through stream for piping.
    */
-  function wait(timeout, options) {
+  function wait(timeout) {
     return map(function(file, cb) {
       setTimeout(function() {
         cb(null, file);
